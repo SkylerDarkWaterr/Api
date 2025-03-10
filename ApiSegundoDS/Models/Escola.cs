d9d0331b07729f00a092b2421baac6de1ab739de
SkylerDarkWaterr <goliveirasantos1302@gmail.com>
2025-03-10 09:07:22 -0300

    feat: Classes Escola, Professor e Aluno

---
+++ ApiSegundoDS/Models/Escola.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiSegundoDS.Models
+{
+    public class Escola
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public Professor Professor { get; set; }
+    }
+}
